2892d24bf97f0507278ac2451fadf5b2d458de08
Nate Morrow <4131420+skillcap@users.noreply.github.com>
2026-04-03 15:58:27 -0500

    Breaking out fastfetch configuration, adding zellij configuration.

--- modules/core/cli.nix
+++ modules/core/cli.nix
@@ -1,15 +1,5 @@
 { config, pkgs, lib, ... }:
 
-let
-  nixos-logo-png = pkgs.runCommand "nixos-logo.png" {
-    buildInputs = [ pkgs.imagemagick ];
-  } ''
-    magick -density 1200 -background none ${pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/master/logo/nix-snowflake-colours.svg";
-      hash = "sha256-43taHBHoFJbp1GrwSQiVGtprq6pBbWcKquSTTM6RLrI=";
-    }} -resize 1000x1000 $out
-  '';
-in
 {
   home.packages = with pkgs; [
     btop
@@ -23,7 +13,10 @@ in
     sd
     starship
     wiki-tui
-    zellij
+  ];
+
+  imports = [
+    ./fastfetch.nix
   ];
 
   programs.fish = {
@@ -39,7 +32,6 @@ in
       ls = "eza";
       l = "eza -la";
       cd = "z";
-      ff = "fastfetch";
     };
     plugins = [
       {
@@ -66,146 +58,42 @@ in
     ];
   };
 
-  programs.fastfetch = {
+  programs.helix = {
     enable = true;
     settings = {
-      logo = {
-        source = "${nixos-logo-png}";
-        type = "kitty-direct";
-        width = 50;
-        height = 25;
-        padding = {
-          top = 1;
-          right = 2;
-        };
+      theme = "tokyonight_transparent";
+      keys.normal = {
+        "C-h" = [
+          ":sh rm -f /tmp/hx-yazi"
+          ":insert-output yazi --chooser-file=/tmp/hx-yazi"
+          ":open %sh{cat /tmp/hx-yazi}"
+          ":redraw"
+        ];
+        "{" = "goto_prev_paragraph";
+        "}" = "goto_next_paragraph";
       };
-      modules = [
-        "break"
-        {
-          type = "custom";
-          format = "${builtins.fromJSON "\"\\u001b\""}[90m┌──────────────────────Hardware──────────────────────┐";
-        }
-        {
-          type = "command";
-          key = " PC";
-          keyColor = "green";
-          text = "hostname";
-        }
-        {
-          type = "cpu";
-          key = "│ ├";
-          keyColor = "green";
-        }
-        {
-          type = "gpu";
-          key = "│ ├󰍛";
-          keyColor = "green";
-        }
-        {
-          type = "memory";
-          key = "│ ├󰍛";
-          keyColor = "green";
-        }
-        {
-          type = "disk";
-          key = "└ └";
-          keyColor = "green";
-        }
-        {
-          type = "custom";
-          format = "${builtins.fromJSON "\"\\u001b\""}[90m└────────────────────────────────────────────────────┘";
-        }
-        "break"
-        {
-          type = "custom";
-          format = "${builtins.fromJSON "\"\\u001b\""}[90m┌──────────────────────Software──────────────────────┐";
-        }
-        {
-          type = "os";
-          key = " OS";
-          keyColor = "yellow";
-        }
-        {
-          type = "kernel";
-          key = "│ ├";
-          keyColor = "yellow";
-        }
-        {
-          type = "bios";
-          key = "│ ├";
-          keyColor = "yellow";
-        }
-        {
-          type = "packages";
-          key = "│ ├󰏖";
-          keyColor = "yellow";
-        }
-        {
-          type = "shell";
-          key = "└ └";
-          keyColor = "yellow";
-        }
-        "break"
-        {
-          type = "de";
-          key = " DE";
-          keyColor = "blue";
-        }
-        {
-          type = "lm";
-          key = "│ ├";
-          keyColor = "blue";
-        }
-        {
-          type = "wm";
-          key = "│ ├";
-          keyColor = "blue";
-        }
-        {
-          type = "wmtheme";
-          key = "│ ├󰉼";
-          keyColor = "blue";
-        }
-        {
-          type = "terminal";
-          key = "└ └";
-          keyColor = "blue";
-        }
-        {
-          type = "custom";
-          format = "${builtins.fromJSON "\"\\u001b\""}[90m└────────────────────────────────────────────────────┘";
-        }
-        "break"
-        {
-          type = "custom";
-          format = "${builtins.fromJSON "\"\\u001b\""}[90m┌──────────────────Uptime / Age / DT─────────────────┐";
-        }
-        {
-          type = "command";
-          key = "  OS Age ";
-          keyColor = "magenta";
-          text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
-        }
-        {
-          type = "uptime";
-          key = "  Uptime ";
-          keyColor = "magenta";
-        }
-        {
-          type = "datetime";
-          key = "  DateTime ";
-          keyColor = "magenta";
-        }
-        {
-          type = "custom";
-          format = "${builtins.fromJSON "\"\\u001b\""}[90m└────────────────────────────────────────────────────┘";
-        }
-        {
-          type = "colors";
-          paddingLeft = 2;
-          symbol = "circle";
-        }
-      ];
+      keys.select = {
+        "{" = "goto_prev_paragraph";
+        "}" = "goto_next_paragraph";
+      };
+    };
+    languages.language = [{
+        name = "nix";
+        auto-format = false;
+        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      }];
+    themes = {
+      tokyonight_transparent = {
+        "inherits" = "tokyonight";
+        "ui.background" = { };
+      };
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+    settings = {
+      theme = "tokyo-night";
     };
   };
 
@@ -241,7 +129,7 @@ in
     shellWrapperName = "y";
     settings = {
       opener = {
-        edit = [ { run = "nvim \"$@\""; block = true; } ];
+        edit = [ { run = "hx \"$@\""; block = true; } ];
       };
       preview = {
         image_delay = 0;
@@ -255,7 +143,7 @@ in
   programs.nushell.enable = true;
 
   home.sessionVariables = {
-    EDITOR = "nvim";
+    EDITOR = "hx";
   };
 
   xdg.configFile = {
