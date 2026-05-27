f2a037de669bba83059f058cc82e890eb87214fd
Nate Morrow <4131420+skillcap@users.noreply.github.com>
2026-05-27 15:06:33 -0500

    Updating hypr monitor Configuration

--- hosts/ANV1L/display.nix
+++ hosts/ANV1L/display.nix
@@ -7,23 +7,22 @@
       # HYPRLAND MONITOR CONFIGURATION
       # ==============================
       monitor=DP-1, 1920x1080@60, 0x1210, 1, transform, 3
-      monitor=DP-2, 2560x1440@165, 1520x0, 1
+      #monitor=DP-2, 2560x1440@165, 1520x0, 1
       monitor=DP-3, 3440x1440@175, 1080x1440, 1, bitdepth, 10, cm, hdredid, sdrbrightness, 1.2, sdrsaturation, 1.15
       monitor=HDMI-A-1, 1920x1080@60, 4520x1210, 1, transform, 1
 
       workspace = 1, monitor:DP-3, default:true
       workspace = 2, monitor:DP-1, default:true
-      workspace = 3, monitor:DP-2, default:true
-      workspace = 4, monitor:HDMI-A-1, default:true
+      #workspace = 3, monitor:DP-2, default:true
+      workspace = 3, monitor:HDMI-A-1, default:true
 
       render {
           direct_scanout = 3
-          cm_fs_passthrough = 1
+          cm_auto_hdr = 1
       }
 
       misc {
           vrr = 2
-          vfr = true
       }
 
       windowrule {
